22953bcd552dbdc0469dd47342a690f91167fdf0
FarihaAhmedRaisa <raisaahmed19@gmail.com>
2025-04-25 18:00:28 +0600

    Add files via upload

---
+++ HW_Lec2.R
@@ -0,0 +1,32 @@
+## Question1
+#This year 2025; University started 2020; Year born 2001
+a= 2025-2020
+b= 2025-2001
+x= a/b
+y= x*100
+#  percentage of my life spent at the university is 20.833% (Oh ho! Never really thought in this way. Not liking it!)
+
+## Question 2
+# Variables and Vectors
+p= 21
+q= 15
+char= c('Fariha', 'Raisa')
+int= c(1,2,3,4,5)
+logical= c('TRUE', 'FALSE')
+
+
+
+## Question 3
+df <- data.frame(
+  Name = c("Raisa", "Bakhtiar", "Sana", "Muna"),
+  Age = c(25, 20, 18, 21),
+  Gender= c("Female", "Male", "Female", "Female"),
+  IsStudent = c(TRUE, TRUE,  FALSE, TRUE)
+)
+summary (df)
+
+
+
+
+
+
